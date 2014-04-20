c1ecf38b06d4f242cb81eedda8982242ce2edbf4
Karthik Ram <karthik.ram@gmail.com>
2014-04-20 11:53:22 -0700

    foo

--- startup/flat-ui/js/application.js
+++ startup/flat-ui/js/application.js
@@ -25,16 +25,18 @@ String.prototype.repeat = function(num) {
   };
 
   $(function() {
-  
-    // Todo list
-    $(".todo").on('click', 'li', function() {
-      $(this).toggleClass("todo-done");
-    });
 
     // Custom Selects
     $("select[name='huge']").selectpicker({style: 'btn-hg btn-primary', menuStyle: 'dropdown-inverse'});
-    $("select[name='herolist']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
+    $("select[name='large']").selectpicker({style: 'btn-lg btn-danger'});
     $("select[name='info']").selectpicker({style: 'btn-info'});
+    $("select[name='small']").selectpicker({style: 'btn-sm btn-warning'});
+
+    // Tabs
+    $(".nav-tabs a").on('click', function (e) {
+      e.preventDefault();
+      $(this).tab("show");
+    })
 
     // Tooltips
     $("[data-toggle=tooltip]").tooltip("show");
@@ -44,14 +46,54 @@ String.prototype.repeat = function(num) {
 
     // jQuery UI Sliders
     var $slider = $("#slider");
-    if ($slider.length) {
+    if ($slider.length > 0) {
       $slider.slider({
         min: 1,
         max: 5,
-        value: 2,
+        value: 3,
         orientation: "horizontal",
         range: "min"
       }).addSliderSegments($slider.slider("option").max);
+    }
+
+    var $slider2 = $("#slider2");
+    if ($slider2.length > 0) {
+      $slider2.slider({
+        min: 1,
+        max: 5,
+        values: [3, 4],
+        orientation: "horizontal",
+        range: true
+      }).addSliderSegments($slider2.slider("option").max);
+    }
+
+    var $slider3 = $("#slider3")
+      , slider3ValueMultiplier = 100
+      , slider3Options;
+
+    if ($slider3.length > 0) {
+      $slider3.slider({
+        min: 1,
+        max: 5,
+        values: [3, 4],
+        orientation: "horizontal",
+        range: true,
+        slide: function(event, ui) {
+          $slider3.find(".ui-slider-value:first")
+            .text("$" + ui.values[0] * slider3ValueMultiplier)
+            .end()
+            .find(".ui-slider-value:last")
+            .text("$" + ui.values[1] * slider3ValueMultiplier);
+        }
+      });
+
+      slider3Options = $slider3.slider("option");
+      $slider3.addSliderSegments(slider3Options.max)
+        .find(".ui-slider-value:first")
+        .text("$" + slider3Options.values[0] * slider3ValueMultiplier)
+        .end()
+        .find(".ui-slider-value:last")
+        .text("$" + slider3Options.values[1] * slider3ValueMultiplier);
     }
 
     var $verticalSlider = $("#vertical-slider");
@@ -65,15 +107,15 @@ String.prototype.repeat = function(num) {
       }).addSliderSegments($verticalSlider.slider("option").max, "vertical");
     }
 
+    // Add style class name to a tooltips
+    $(".tooltip").addClass(function() {
+      if ($(this).prev().attr("data-tooltip-style")) {
+        return "tooltip-" + $(this).prev().attr("data-tooltip-style");
+      }
+    });
+
     // Placeholders for input/textarea
     $(":text, textarea").placeholder();
-
-    // Focus state for append/prepend inputs
-    $('.input-group').on('focus', '.form-control', function () {
-      $(this).closest('.input-group, .form-group').addClass('focus');
-    }).on('blur', '.form-control', function () {
-      $(this).closest('.input-group, .form-group').removeClass('focus');
-    });
 
     // Make pagination demo work
     $(".pagination").on('click', "a", function() {
@@ -89,10 +131,75 @@ String.prototype.repeat = function(num) {
       e.preventDefault();
     });
 
+    // jQuery UI Spinner
+    $.widget( "ui.customspinner", $.ui.spinner, {
+      widgetEventPrefix: $.ui.spinner.prototype.widgetEventPrefix,
+      _buttonHtml: function() { // Remove arrows on the buttons
+        return "" +
+        "<a class='ui-spinner-button ui-spinner-up ui-corner-tr'>" +
+          "<span class='ui-icon " + this.options.icons.up + "'></span>" +
+        "</a>" +
+        "<a class='ui-spinner-button ui-spinner-down ui-corner-br'>" +
+          "<span class='ui-icon " + this.options.icons.down + "'></span>" +
+        "</a>";
+      }
+    });
+
+    $('#spinner-01, #spinner-02, #spinner-03, #spinner-04, #spinner-05').customspinner({
+      min: -99,
+      max: 99
+    }).on('focus', function () {
+      $(this).closest('.ui-spinner').addClass('focus');
+    }).on('blur', function () {
+      $(this).closest('.ui-spinner').removeClass('focus');
+    });
+
+
+    // Focus state for append/prepend inputs
+    $('.input-group').on('focus', '.form-control', function () {
+      $(this).closest('.input-group, .form-group').addClass('focus');
+    }).on('blur', '.form-control', function () {
+      $(this).closest('.input-group, .form-group').removeClass('focus');
+    });
+
+    // Table: Toggle all checkboxes
+    $('.table .toggle-all').on('click', function() {
+      var ch = $(this).find(':checkbox').prop('checked');
+      $(this).closest('.table').find('tbody :checkbox').checkbox(!ch ? 'check' : 'uncheck');
+    });
+
+    // Table: Add class row selected
+    $('.table tbody :checkbox').on('check uncheck toggle', function (e) {
+      var $this = $(this)
+        , check = $this.prop('checked')
+        , toggle = e.type == 'toggle'
+        , checkboxes = $('.table tbody :checkbox')
+        , checkAll = checkboxes.length == checkboxes.filter(':checked').length
+
+      $this.closest('tr')[check ? 'addClass' : 'removeClass']('selected-row');
+      if (toggle) $this.closest('.table').find('.toggle-all :checkbox').checkbox(checkAll ? 'check' : 'uncheck');
+    });
+
+    // jQuery UI Datepicker
+    var datepickerSelector = '#datepicker-01';
+    $(datepickerSelector).datepicker({
+      showOtherMonths: true,
+      selectOtherMonths: true,
+      dateFormat: "d MM, yy",
+      yearRange: '-1:+1'
+    }).prev('.btn').on('click', function (e) {
+      e && e.preventDefault();
+      $(datepickerSelector).focus();
+    });
+    $.extend($.datepicker, {_checkOffset:function(inst,offset,isFixed){return offset}});
+
+    // Now let's align datepicker with the prepend button
+    $(datepickerSelector).datepicker('widget').css({'margin-left': -$(datepickerSelector).prev('.input-group-btn').find('.btn').outerWidth()});
+
     // Switch
     $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
 
-        // Typeahead
+    // Typeahead
     if($('#typeahead-demo-01').length) {
       $('#typeahead-demo-01').typeahead({
         name: 'states',
@@ -104,11 +211,9 @@ String.prototype.repeat = function(num) {
         "North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina",
         "South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
       });
-    }  
+    }    
 
     // make code pretty
     window.prettyPrint && prettyPrint();
-    
   });
-  
 })(jQuery);
