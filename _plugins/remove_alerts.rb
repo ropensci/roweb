4bd131ad6f17bd4f53d35b9fd6c833f0cd909f06
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-14 07:25:12 -0700

    changed nokogiri to oga

--- _plugins/remove_alerts.rb
+++ _plugins/remove_alerts.rb
@@ -2,15 +2,21 @@
 # Author: Scott Chamberlain, @sckott
 # License: MIT
 
-require 'nokogiri'
+require 'oga'
 
 module Jekyll
   module RemoveAlerts
     def remove_alerts(input)
-      res = Nokogiri::HTML(input)
+    	res = Oga.parse_html(input)
+      # res = Nokogiri::HTML(input)
       res.xpath('//div[@role="alert"]').remove
       res
     end
   end
 end
 Liquid::Template.register_filter(Jekyll::RemoveAlerts)
+
+# require 'oga'
+# handle = File.open('/Users/sacmac/github/ropensci/roweb/_site/blog/2014/02/18/antweb/index.html')
+# res = Oga.parse_html(handle)
+# res.xpath('//div[@role="alert"]')
