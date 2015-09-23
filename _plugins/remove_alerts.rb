e16839c20b6a5abdcb5725566f8b1c686d2f1f43
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-23 14:34:25 -0700

    woops, alerts were getting through, switched back to nokogiri #201 I guess nokogiri was maybe never a problem

--- _plugins/remove_alerts.rb
+++ _plugins/remove_alerts.rb
@@ -2,21 +2,15 @@
 # Author: Scott Chamberlain, @sckott
 # License: MIT
 
-require 'oga'
+require 'nokogiri'
 
 module Jekyll
   module RemoveAlerts
     def remove_alerts(input)
-    	res = Oga.parse_html(input)
-      # res = Nokogiri::HTML(input)
+      res = Nokogiri::HTML(input)
       res.xpath('//div[@role="alert"]').remove
       res
     end
   end
 end
 Liquid::Template.register_filter(Jekyll::RemoveAlerts)
-
-# require 'oga'
-# handle = File.open('/Users/sacmac/github/ropensci/roweb/_site/blog/2014/02/18/antweb/index.html')
-# res = Oga.parse_html(handle)
-# res.xpath('//div[@role="alert"]')
